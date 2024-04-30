72f4f4a2aed4f74c5049dfa2d9219b245a6ef82d
dddogo <125663232+diogorodrigu@users.noreply.github.com>
2024-04-30 11:50:43 -0300

    Add files via upload

---
+++ front-corr-txt/src/paginas/bancos.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Bancos(){
+  return (
+    <div>
+        <h2>Banco</h2>
+      <h2>Esta é o Banco, passa o money</h2>
+    </div>
+  );
+}
+
+
+export default Bancos;
